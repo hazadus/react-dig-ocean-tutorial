2f5af6bb44b285f95a179a91dcbd4556f4324edb
hazadus <hazadus7@gmail.com>
2022-10-14 14:32:06 +0300

    Step #2 complete. Custom component added, Bootstrap connected.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-// https://www.digitalocean.com/community/tutorials/how-to-set-up-a-react-project-with-create-react-app
-//
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// import { Container } from 'react-bootstrap';
+import Badge from 'react-bootstrap/Badge';
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(properties) {
+    super(properties);
+    this.state  = {
+      date: new Date()
+    };
+  }
+
+  // So called "Lifecycle Methods":
+  // Called whenever Clock is rendered to the DOM for the first time
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  // Called when Clock is removed from DOM
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  // Update the time object
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <Badge bg="success">{this.state.date.toLocaleTimeString()}</Badge>
+    );
+  }
+}
+
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: "\u{1F338}",
+    name: "thunderbolt"
+  },
+  {
+    emoji: "\u{1F389}",
+    name: "party popper"
+  },
+  {
+    emoji: "\u{1F30F}",
+    name: "grinning face"
+  },
+]
+const displayAction = false;
 
 function App() {
+  const greeting = "greeting";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <strong>React</strong> world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Use <> and </> as wrapper because function 
+    // must return only one HTML element.
+    <div className="container">
+      <h1 id={greeting}>
+        Hello <strong>React</strong> world! <Badge bg="secondary">New!</Badge>
+      </h1>
+      <p>
+        Writing HTML exactly from JavaScript!
+      </p>
+      {displayAction &&
+        <p>This is hidden.</p>
+      }
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button onClick={displayEmojiName}>
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+      <Clock />
     </div>
   );
 }
